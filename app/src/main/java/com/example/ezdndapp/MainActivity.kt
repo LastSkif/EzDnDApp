d1c6e3464789714a787b7e5c7a71d5ba6e4953f8
Stultum <tgslaayer@gmail.com>
2020-09-05 15:51:05 +0700

    #2 Added BottomNav

--- app/src/main/java/com/example/ezdndapp/MainActivity.kt
+++ app/src/main/java/com/example/ezdndapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.ezdndapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.page_2 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
+        }
     }
+    
+    
 }
