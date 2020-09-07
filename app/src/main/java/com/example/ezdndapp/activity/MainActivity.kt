1a7116d96d94a2d0bf5fc23930e7f560dfd4edb7
Stultum <tgslaayer@gmail.com>
2020-09-07 11:27:59 +0700

    #2 Trying to fix BottomNav

---
+++ app/src/main/java/com/example/ezdndapp/activity/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ezdndapp.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.ezdndapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainActivityFragment) as NavHostFragment
+        val navController = host.navController
+        setUpBottomNav(navController)
+    }
+
+    private fun setUpBottomNav(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.mainActivityNavigationView)
+        bottomNav?.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/ezdndapp/fragment/InventoryFragment.kt
@@ -0,0 +1,5 @@
+package com.example.ezdndapp.fragment
+
+import androidx.fragment.app.Fragment
+
+class InventoryFragment : Fragment()
